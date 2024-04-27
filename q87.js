e3574fde09ba0f0a2186b94baf81d46e87969af2
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-28 02:29:34 +0500

    File of q87

---
+++ q87.js
@@ -0,0 +1,11 @@
+//Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+function extractFirstTenChars(str) {
+    return str.substring(0, 10);
+}
+console.log(extractFirstTenChars("Hello,JavaScript world!"));
+// This function takes out the first 10 characters from any text
+//function extractFirstTenChars(str: string): string {
+//  return str.substring(0, 10); // Gets characters from start to position 10
+// Example: Taking the first 10 characters of a sentence
+//console.log(extractFirstTenChars("Hello, JavaScript world!")); // Shows the first 10 characters
+// It gives us a smaller piece of the original text, just the beginning part.
---
+++ q87.ts
@@ -0,0 +1,14 @@
+//Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+
+
+
+function extractFirstTenChars(str: string):string {
+    return str.substring(0,10);
+}
+
+console.log(extractFirstTenChars("Hello,JavaScript world!"));
+
+// Shows the first 10 characters  (Hello,Java)
+
+
+
