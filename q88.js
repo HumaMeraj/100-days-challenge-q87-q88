3c5a9164c59d58a4110ac790492be206aca0b28b
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-28 02:30:02 +0500

    File of q88

---
+++ q88.js
@@ -0,0 +1,7 @@
+//Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+// This function rounds a decimal number to the nearest whole number
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(4.7)); // Outputs: 5
+console.log(roundToNearestInteger(4.4)); // Outputs: 4
---
+++ q88.ts
@@ -0,0 +1,13 @@
+//Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+
+// This function rounds a decimal number to the nearest whole number
+
+function roundToNearestInteger(num :number): number {
+    return Math.round(num);
+}
+
+console.log (roundToNearestInteger(4.7));    // Outputs: 5
+console.log(roundToNearestInteger(4.4));      // Outputs: 4
+
+
+
